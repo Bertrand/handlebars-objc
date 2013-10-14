cc4cab755f4f67bfb46de78c9bb0cbc66c5ece84
Bertrand Guiheneuf <bertrand@fotonauts.com>
2013-10-14 10:44:59 +0200

    add cocoa pod embedding test project

---
+++ src/non-unit-tests/test-embedding-in-projects/Test OSX CocoaPods/Test OSX CocoaPods/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test OSX CocoaPods
+//
+//  Created by Bertrand Guiheneuf on 10/14/13.
+//  Copyright (c) 2013 Fotonauts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
