96da6fdad20491e3e6c8c05215d1a7dcf9e91f77
Bertrand Guiheneuf <bertrand@fotonauts.com>
2013-10-14 23:12:13 +0200

    update github pages

--- src/utils/generate_github_pages.sh
+++ src/utils/generate_github_pages.sh
@@ -3,11 +3,11 @@
 SCRIPT_DIR=`dirname $0`
 ROOT_DIR="$SCRIPT_DIR/../.."
 
-git diff --quiet --exit-code
-if [ $? -ne 0 ]; then 
-    echo "Cannot generate static github pages if you have uncommited changes" 
-    exit -1;
-fi
+# git diff --quiet --exit-code
+# if [ $? -ne 0 ]; then 
+#     echo "Cannot generate static github pages if you have uncommited changes" 
+#     exit -1;
+# fi
 
 current_branch=`git rev-parse --abbrev-ref HEAD`
 if [ "$current_branch" == "HEAD" ]; then
@@ -25,7 +25,7 @@ pushd .
 cd "$ROOT_DIR"
 
 git checkout gh-pages
-cp -RPf "$ROOT_DIR/"* . 
+cp -RPf "$TMP_FOLDER/*" . 
 git commit -a -m "update github pages" 
 git push 
 
