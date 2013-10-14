4151278e12da06e725b4c1cdcd8c771bd79c04e9
Bertrand Guiheneuf <bertrand@fotonauts.com>
2013-10-14 13:54:14 +0200

    add ios app to CocoaPods test project, and make sure double-architecture project works

---
+++ src/non-unit-tests/test-embedding-in-projects/Test OSX CocoaPods/Test iOS Application With CocoaPods/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test iOS Application With CocoaPods
+//
+//  Created by Bertrand Guiheneuf on 10/14/13.
+//  Copyright (c) 2013 Fotonauts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
