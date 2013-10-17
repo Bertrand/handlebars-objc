f6c390016e684d02ab6c2f4958efd6ad14b20f37
Bertrand Guiheneuf <bertrand@fotonauts.com>
2013-10-17 13:08:31 +0200

    improve static pages generation script

--- src/utils/generate_github_pages.sh
+++ src/utils/generate_github_pages.sh
@@ -9,6 +9,12 @@ if [ $? -ne 0 ]; then
     exit -1;
 fi
 
+GIT_STATUS=`git status -s | grep -e "^\?\?"`
+if [ "$GIT_STATUS" != "" ]; then
+    echo "cannot swith to gh-pages branch if you have untracked files in your current branch."
+    exit -1
+fi
+
 current_branch=`git rev-parse --abbrev-ref HEAD`
 if [ "$current_branch" == "HEAD" ]; then
     echo "Cannot generate static github pages if you're not on a branch"
@@ -25,8 +31,16 @@ pushd .
 cd "$ROOT_DIR"
 
 git checkout gh-pages
-cp -RPf "$TMP_FOLDER/"* . 
-git commit -a -m "update github pages" 
+cp -RPf "$TMP_FOLDER/"* .
+
+GIT_STATUS=`git status -s | grep -e "^\?\?"`
+if [ "$GIT_STATUS" != "" ]; then
+    echo "Cannot commit gh-pages branch if it contains untracked files. Add the new files if needed and commit your changes."
+    echo "You are now on gh-pages branch."
+    exit -1
+fi
+
+git commit -a -m "update github pages"
 git push 
 
 git checkout "$current_branch"
