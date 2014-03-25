929306b53a04b7af0d175bdafd0ee46763701ec2
Bertrand Guiheneuf <bertrand@fotonauts.com>
2014-03-25 22:44:40 +0100

    forgotten file

---
+++ src/handlebars-objcTests/HBTestData.m
@@ -0,0 +1,78 @@
+//
+//  HBTestData.m
+//  handlebars-objc
+//
+//  Created by Bertrand Guiheneuf on 3/25/14.
+//  Copyright (c) 2014 Fotonauts. All rights reserved.
+//
+
+
+#import "HBTestCase.h"
+#import "HBHandlebars.h"
+
+
+@interface HBTestData : HBTestCase
+
+@end
+
+@implementation HBTestData
+
+- (HBHelperBlock) letHelper
+{
+    return ^(HBHelperCallingInfo* callingInfo) {
+        
+        HBDataContext* currentDataContext = callingInfo.data;
+        HBDataContext* descendantDataContext = currentDataContext ? [currentDataContext copy] : [HBDataContext new];
+        
+        for (NSString* paramName in callingInfo.namedParameters) {
+            descendantDataContext[paramName] = callingInfo.namedParameters[paramName];
+        }
+        
+        return callingInfo.statements(callingInfo.context, descendantDataContext);
+    };
+}
+
+- (HBHelperBlock) helloHelper
+{
+    return ^(HBHelperCallingInfo* callingInfo) {
+        return [NSString stringWithFormat:@"Hello %@", callingInfo[@"noun"]];
+    };
+}
+
+- (void) testDeepAtFooTriggersAutomaticTopLevelData
+{
+    NSError* error = nil;
+    id string = @"{{#let world='world'}}{{#if foo}}{{#if foo}}Hello {{@world}}{{/if}}{{/if}}{{/let}}";
+    id hash = @{ @"foo" : @true };
+    NSDictionary* helpers = @{ @"let" : [self letHelper]};
+    
+    NSString* result = [self renderTemplate:string withContext:hash withHelpers:helpers error:&error];
+    XCTAssert(!error, @"evaluation should not generate an error");
+    XCTAssertEqualObjects(result, @"Hello world");
+}
+
+- (void) testDataIsInheritedDownstream
+{
+    NSError* error = nil;
+    id string = @"{{#let foo=1 bar=2}}{{#let foo=bar.baz}}{{@bar}}{{@foo}}{{/let}}{{@foo}}{{/let}}";
+    id hash = @{ @"bar": @{ @"baz": @"hello world" } };
+    NSDictionary* helpers = @{ @"let" : [self letHelper]};
+    
+    NSString* result = [self renderTemplate:string withContext:hash withHelpers:helpers error:&error];
+    XCTAssert(!error, @"evaluation should not generate an error");
+    XCTAssertEqualObjects(result, @"2hello world1");
+}
+
+- (void) testTheRootContextCanBeLookedUpViaAtRoot
+{
+    NSError* error = nil;
+    id string = @"{{@root.foo}}";
+    id hash = @{ @"foo" : @"hello" };
+    NSDictionary* helpers = @{ @"let" : [self letHelper]};
+    
+    NSString* result = [self renderTemplate:string withContext:hash withHelpers:helpers error:&error];
+    XCTAssert(!error, @"evaluation should not generate an error");
+    XCTAssertEqualObjects(result, @"hello");
+}
+
+@end
