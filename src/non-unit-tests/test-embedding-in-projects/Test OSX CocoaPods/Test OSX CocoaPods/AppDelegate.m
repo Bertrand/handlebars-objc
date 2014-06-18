4072c100a31dcc71d3ebe5e255f852b93920727c
Bertrand Guiheneuf <bertrand@fotonauts.com>
2014-06-18 16:49:56 +0200

    tests issue9

--- src/non-unit-tests/test-embedding-in-projects/Test OSX CocoaPods/Test OSX CocoaPods/AppDelegate.m
+++ src/non-unit-tests/test-embedding-in-projects/Test OSX CocoaPods/Test OSX CocoaPods/AppDelegate.m
@@ -11,8 +11,26 @@
 
 @implementation AppDelegate
 
+- (void) testIssue9
+{
+    HBHelperBlock sentenceCaseBlock = ^(HBHelperCallingInfo * callingInfo) {
+        NSString * locale = callingInfo[@"locale"];
+        NSString * src = callingInfo[0];
+        return [src capitalizedStringWithLocale:[[NSLocale alloc] initWithLocaleIdentifier:locale]];
+    };
+    [HBHandlebars registerHelperBlock:sentenceCaseBlock forName:@"sentenceCase"];
+    
+    NSError * error;
+    NSString * result = [HBHandlebars renderTemplateString:@"hello {{sentenceCase value locale='en_US'}}!"
+                               withContext:@{@"value" : @"mike this is your friend george"}
+                                     error:&error];
+    NSLog(@"result : %@", result);
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    [self testIssue9];
+    
     NSString* evaluatedTemplate = [HBHandlebars renderTemplateString:@"Hello from {{value}}" withContext:@{ @"value" : @"Handlebars" } error:nil];
     NSLog(@"%@", evaluatedTemplate);
 }
--- src/non-unit-tests/test-embedding-in-projects/Test OSX CocoaPods/Test iOS Application With CocoaPods/AppDelegate.m
+++ src/non-unit-tests/test-embedding-in-projects/Test OSX CocoaPods/Test iOS Application With CocoaPods/AppDelegate.m
@@ -11,8 +11,27 @@
 
 @implementation AppDelegate
 
+
+- (void) testIssue9
+{
+    HBHelperBlock sentenceCaseBlock = ^(HBHelperCallingInfo * callingInfo) {
+        NSString * locale = callingInfo[@"locale"];
+        NSString * src = callingInfo[0];
+        return [src capitalizedStringWithLocale:[[NSLocale alloc] initWithLocaleIdentifier:locale]];
+    };
+    [HBHandlebars registerHelperBlock:sentenceCaseBlock forName:@"sentenceCase"];
+    
+    NSError * error;
+    NSString * result = [HBHandlebars renderTemplateString:@"hello {{sentenceCase value locale='en_US'}}!"
+                               withContext:@{@"value" : @"mike this is your friend george"}
+                                     error:&error];
+    NSLog(@"result : %@", result);
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+   [self testIssue9];
+
     NSString* evaluatedTemplate = [HBHandlebars renderTemplateString:@"Hello from {{value}}" withContext:@{ @"value" : @"Handlebars" } error:nil];
     NSLog(@"%@", evaluatedTemplate);
     
